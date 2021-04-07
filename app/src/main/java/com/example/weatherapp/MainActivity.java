c861ba07067ec07969314afe732ddbbf12c1c01f
bitaaz <bb_az@yahoo.com>
2021-04-07 21:57:42 +0430

    add code for retrieve data from viewModel

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.weatherapp.model.WeatherModel;
+import com.example.weatherapp.viewmodel.WeatherViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WeatherViewModel weatherViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        weatherViewModel = new ViewModelProvider(this).get(WeatherViewModel.class);
+
+
+        observer();
+        weatherApi("Tehran");
+
+
+
+
+    }
+
+    public void observer(){
+
+        weatherViewModel.getWeather().observe(this, new Observer<WeatherModel>() {
+            @Override
+            public void onChanged(WeatherModel weatherModel) {
+
+                if (weatherModel != null){
+
+                    Log.v("Tag", "name: " + weatherModel.getMain().getTemp());
+                }
+
+            }
+        });
+
+
+
+    }
+
+    public void weatherApi(String query){
+
+        weatherViewModel.weatherApi(query);
     }
 }
