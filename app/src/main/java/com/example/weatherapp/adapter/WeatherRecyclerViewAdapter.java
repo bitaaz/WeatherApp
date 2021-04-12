378f11779af5f51aa37223faa07516cb7c86d944
bitaaz <bb_az@yahoo.com>
2021-04-12 14:59:39 +0430

    Add getSelectedWeather method to get model of the selected weather

--- app/src/main/java/com/example/weatherapp/adapter/WeatherRecyclerViewAdapter.java
+++ app/src/main/java/com/example/weatherapp/adapter/WeatherRecyclerViewAdapter.java
@@ -1,10 +1,14 @@
 package com.example.weatherapp.adapter;
 
+import android.os.Build;
+import android.text.format.Time;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -12,16 +16,14 @@ import com.example.weatherapp.R;
 import com.example.weatherapp.model.Weather;
 import com.example.weatherapp.model.WeatherModel;
 
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
+
 
 import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -32,6 +34,11 @@ import java.util.TimeZone;
 public class WeatherRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private List<WeatherModel> weathers = new ArrayList<>();
+    private OnWeatherItemListener onWeatherItemListener;
+
+    public WeatherRecyclerViewAdapter(OnWeatherItemListener onWeatherItemListener) {
+        this.onWeatherItemListener = onWeatherItemListener;
+    }
 
     public void setWeathers(List<WeatherModel> weathers) {
         this.weathers = weathers;
@@ -42,8 +49,9 @@ public class WeatherRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerVie
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.weather_item, parent, false);
-        return new WeatherViewHolder(view);
+        return new WeatherViewHolder(view, onWeatherItemListener);
     }
+
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
@@ -53,7 +61,6 @@ public class WeatherRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerVie
 
         DecimalFormat decimalFormat = new DecimalFormat("#.#");
         String temperature = decimalFormat.format(weathers.get(position).getMain().getTemp() - 273.15);
-
 
         ((WeatherViewHolder)holder).temperature.setText(temperature + "°C");
 
@@ -68,5 +75,16 @@ public class WeatherRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerVie
     @Override
     public int getItemCount() {
         return weathers.size();
+    }
+
+    public WeatherModel getSelectedWeather(int position){
+
+        if (weathers != null){
+            if (weathers.size() > 0){
+                return weathers.get(position);
+            }
+        }
+
+        return null;
     }
 }
