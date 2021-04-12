f94ab12f59c750f298dbf0731c12fed0d998e0a0
bitaaz <bb_az@yahoo.com>
2021-04-12 15:00:33 +0430

    Add listener to each row of the recyclerView

--- app/src/main/java/com/example/weatherapp/adapter/WeatherViewHolder.java
+++ app/src/main/java/com/example/weatherapp/adapter/WeatherViewHolder.java
@@ -10,16 +10,28 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.weatherapp.R;
 
 
-public class WeatherViewHolder extends RecyclerView.ViewHolder {
+public class WeatherViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
     TextView location, temperature;
     ImageView imageView;
 
-    public WeatherViewHolder(@NonNull View itemView) {
+    OnWeatherItemListener onWeatherItemListener;
+
+    public WeatherViewHolder(@NonNull View itemView, OnWeatherItemListener onWeatherItemListener) {
         super(itemView);
+
+        this.onWeatherItemListener = onWeatherItemListener;
 
         location = itemView.findViewById(R.id.location);
         temperature = itemView.findViewById(R.id.temperature);
         imageView = itemView.findViewById(R.id.pic);
+
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        onWeatherItemListener.onWeatherItemClickListener(getAdapterPosition());
     }
 }
