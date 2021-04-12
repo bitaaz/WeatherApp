8bf193193c791c76123b2c997e35c292b3f60835
bitaaz <bb_az@yahoo.com>
2021-04-12 14:54:08 +0430

    Add on weather item click listener to show weather details

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -6,19 +6,26 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.weatherapp.adapter.OnWeatherItemListener;
 import com.example.weatherapp.adapter.WeatherRecyclerViewAdapter;
 import com.example.weatherapp.model.WeatherModel;
 import com.example.weatherapp.viewmodel.WeatherViewModel;
 
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnWeatherItemListener {
 
     private WeatherViewModel weatherViewModel;
 
@@ -42,7 +49,9 @@ public class MainActivity extends AppCompatActivity {
 
         observer();
         configureRecyclerView();
-        weatherApi("Tokyo");
+        weatherApi("Tehran");
+
+
 
 
 
@@ -80,8 +89,57 @@ public class MainActivity extends AppCompatActivity {
 
     private void configureRecyclerView(){
 
-        adapter = new WeatherRecyclerViewAdapter();
+        adapter = new WeatherRecyclerViewAdapter(this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public void onWeatherItemClickListener(int position) {
+
+        Intent intent = new Intent(MainActivity.this, WeatherDetailsActivity.class);
+
+        intent.putExtra("weather", adapter.getSelectedWeather(position));
+
+
+        WeatherModel weatherModel = adapter.getSelectedWeather(position);
+
+        intent.putExtra("name", weatherModel.getName());
+
+        DecimalFormat decimalFormat = new DecimalFormat("#.#");
+        String temperature = decimalFormat.format(weatherModel.getMain().getTemp() - 273.15);
+        String maxTemp = decimalFormat.format(weatherModel.getMain().getTemp_max() - 273.15);
+        String minTemp = decimalFormat.format(weatherModel.getMain().getTemp_min() - 273.15);
+
+        intent.putExtra("temp", temperature);
+        intent.putExtra("maxTemp", maxTemp);
+        intent.putExtra("minTemp", minTemp);
+
+        intent.putExtra("wind", weatherModel.getWind().getSpeed());
+        intent.putExtra("condition", weatherModel.getWeather().get(0).getMain());
+        intent.putExtra("desc", weatherModel.getWeather().get(0).getDescription());
+        intent.putExtra("pressure", weatherModel.getMain().getPressure());
+        intent.putExtra("humid", weatherModel.getMain().getHumidity());
+
+        DateFormat dateFormat = DateFormat.getTimeInstance();
+        String sunriseTime = dateFormat.format(new Date(weatherModel.getSys().getSunrise()));
+        String sunsetTime = dateFormat.format(new Date(weatherModel.getSys().getSunset()));
+        String lastUpdated = dateFormat.format(new Date(weatherModel.getDt()));
+
+        intent.putExtra("sunrise", sunriseTime);
+        intent.putExtra("sunset", sunsetTime);
+        intent.putExtra("updated", lastUpdated);
+
+        intent.putExtra("icon", weatherModel.getWeather().get(0).getIcon());
+
+
+
+
+
+
+        startActivity(intent);
+
+
+
     }
 }
